e31bfaaa76f6184686936f5313165a12b0534a6f
geekbrains <4091709@mail.ru>
2022-05-15 22:30:43 +0300

    В процессе

--- LessonHomework_2_5/Program.cs
+++ LessonHomework_2_5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lucene.Net.Support;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,14 @@ namespace LessonHomework_2_5
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число от 1 до 3 ");
+            string Number = Console.ReadLine();
+            Console.ReadKey();
         }
+
     }
 }
