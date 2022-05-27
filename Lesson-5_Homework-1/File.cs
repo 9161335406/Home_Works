10c048f21bcd671b5b0216e542fa9c85d29da60b
geekbrains <4091709@mail.ru>
2022-05-27 15:17:28 +0300

    Правка.

---
+++ Lesson-5_Homework-1/File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+ulong System IO;
+
+namespace Lesson_5_Homework_1
+{
+    class File
+    {
+        public Files()
+        {
+            string filename = text.text.file;
+
+            File.WriteAllText(filename, "str");
+
+            File.AppendAllText(filename,
+        }
+
+       
+        
+    }
+}
