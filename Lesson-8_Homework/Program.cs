59c3ae06419f0921ca9f70c23e5589c3da0e6610
geekbrains <4091709@mail.ru>
2022-06-08 21:23:29 +0300

    Откомпилировано.

--- Lesson-8_Homework/Program.cs
+++ Lesson-8_Homework/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
+﻿using HomeworkMyUtils;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lesson_8_Homework
 {
-    class Program
+    internal class Sample02
     {
+
         static void Main(string[] args)
         {
+            OutputHelpers.PrintInfo(8, "Разумных Игорь Александрович");
+            Console.Title = Properties.Settings.Default.ApplicationName;
+
+            Console.WriteLine(Properties.Settings.Default.UsersSettings1);
+            Console.WriteLine(Properties.Settings.Default.UsersSettings2);
+
+            Properties.Settings.Default.UsersSettings1 = 1000;
+            Properties.Settings.Default.UsersSettings2 = "Hello student";
+            Properties.Settings.Default.Save();
+
+            Console.WriteLine(Properties.Settings.Default.UsersSettings1);
+            Console.WriteLine(Properties.Settings.Default.UsersSettings1);
+
+            Console.ReadKey(true);
+            
         }
     }
 }
+
+
