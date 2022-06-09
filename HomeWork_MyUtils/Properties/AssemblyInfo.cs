f098e550ec99380fafb4cffbc098a6f9ea73c0ca
geekbrains <4091709@mail.ru>
2022-06-10 00:25:06 +0300

    Работа над запросом

---
+++ HomeWork_MyUtils/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HomeWork_MyUtils")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWork_MyUtils")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a6d5b16c-1be3-4299-b276-c91f0968aef5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lesson-8_Homework1/Program.cs
@@ -0,0 +1,53 @@
+﻿using HomeWork_MyUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            OutputHelpers.PrintInfo(8, "Разумных Игорь Александрович");
+
+//#if DEBUG
+
+//            Console.Title = Properties.Settings.Default.ApplicationNameDebug;
+
+//#else
+ 
+//            Console.Title = Properties.Settings.Default.ApplicationName;
+
+
+//#endif
+//            Console.WriteLine($"Настройка UserSettings1: {Properties.Settings.Default.UserSettings1}");
+//            Console.WriteLine($"Настройка UserSettings2: {Properties.Settings.Default.UserSettings2}");
+
+//            Console.Write("Укажите значение UserSettings1: ");
+//            Properties.Settings.Default.UserSettings1 = int.Parse(Console.ReadLine());
+//            Properties.Settings.Default.Save();
+
+//            Console.WriteLine();
+
+//            Console.Write("Укажите значение UserSettings2: ");
+//            Properties.Settings.Default.UserSettings2 = Console.ReadLine();
+            
+//            //Properties.Settings.Default.UserSettings1 = 100;
+//            //Properties.Settings.Default.UserSettings2 = "MySettings"; 
+
+//            Console.WriteLine($"Настройка UserSettings1: {Properties.Settings.Default.UserSettings1}");
+//            Console.WriteLine($"Настройка UserSettings2: {Properties.Settings.Default.UserSettings2}");
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+
+}
+    
